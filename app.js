0f792d11094e9e1c0ad6a059431bbcd38fd98d57
bjwine0 <bradsmilek@gmail.com>
2019-10-26 00:14:25 -0700

    have buttons showing and rendering new unicorns in proper place// need to add conditionals for other locations and handler for the barn

--- app.js
+++ app.js
@@ -40,6 +40,22 @@ function Unicorn(name, color, food, location) {
     var sec = document.getElementById(this.location);
     div.append(ul);
     sec.appendChild(div);
+
+    if(this.location === 'barn'){
+
+      var pasture = document.createElement('button');
+      pasture.id = this.name;
+      pasture.textContent = 'Take to Pasture';
+      pasture.addEventListener('click', goToPasture); //need function
+      ul.appendChild(pasture);
+
+      var trail = document.createElement('button');
+      trail.id = this.name;
+      trail.textContent = 'Take on Trail';
+      trail.addEventListener('click', goToTrail); //need function
+      ul.appendChild(trail);
+
+    }
     
   }
   allUnicorns.push(this);
@@ -48,18 +64,18 @@ function Unicorn(name, color, food, location) {
 
 // calling constructor and storing new instance in variable 
 var unicornOne = new Unicorn('henry', 'blue', 'corn', 'barn');
-var unicornTwo = new Unicorn('john', 'orange', 'apples', 'barn');
-var unicornThree = new Unicorn('susan', 'green','grapes', 'barn');
-var unicornFour = new Unicorn('jacky', 'red', 'bananas', 'barn');
+// var unicornTwo = new Unicorn('john', 'orange', 'apples', 'barn');
+// var unicornThree = new Unicorn('susan', 'green','grapes', 'barn');
+// var unicornFour = new Unicorn('jacky', 'red', 'bananas', 'barn');
 
 console.log(unicornOne);
 console.log(allUnicorns)
 
 // calling render method on instance to show unicorns
 unicornOne.render();
-unicornTwo.render();
-unicornThree.render();
-unicornFour.render();
+// unicornTwo.render();
+// unicornThree.render();
+// unicornFour.render();
 
 
 //maybe buttons as options
@@ -70,5 +86,34 @@ unicornFour.render();
 // stretch  db  talk to db  deply with links
 
 
+function goToPasture(event) {
 
+  console.log('pasture')
+  console.log(event.target)
+  var id = event.target.id;
+  for ( var i = 0; i < allUnicorns.length; i++) {
+    if( id === allUnicorns[i].name) {
+    
+      allUnicorns[i].location = 'pasture';
+      console.log(allUnicorns)
+      allUnicorns[i].render();
+    }
+  }
+}
 
+function goToTrail(event) {
+
+  console.log('trail')
+  console.log(event.target.id);
+  var id = event.target.id;
+
+  for ( var i = 0; i < allUnicorns.length; i++) {
+    if( id === allUnicorns[i].name) {
+      
+      allUnicorns[i].location = 'trail';
+      console.log(allUnicorns)
+      allUnicorns[i].render();
+      
+    }
+  }
+}
