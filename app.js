5c15eaccf0f86cf7c94e6a0f63ba9cc60f6f6f09
bjwine0 <bradsmilek@gmail.com>
2019-10-26 00:30:57 -0700

    buttons good eventhandlers good  copy paste change names everything rendering right

--- app.js
+++ app.js
@@ -55,6 +55,33 @@ function Unicorn(name, color, food, location) {
       trail.addEventListener('click', goToTrail); //need function
       ul.appendChild(trail);
 
+    }else if (this.location === 'pasture'){
+
+      var barn = document.createElement('button');
+      barn.id = this.name;
+      barn.textContent = 'Take to Barn';
+      barn.addEventListener('click', goToBarn); //need function
+      ul.appendChild(barn);
+
+      var trail = document.createElement('button');
+      trail.id = this.name;
+      trail.textContent = 'Take on Trail';
+      trail.addEventListener('click', goToTrail);
+      ul.appendChild(trail);
+
+    }else if (this.location === 'trail'){
+
+      var barn = document.createElement('button');
+      barn.id = this.name;
+      barn.textContent = 'Take to Barn';
+      barn.addEventListener('click', goToBarn);
+      ul.appendChild(barn);
+
+      var pasture = document.createElement('button');
+      pasture.id = this.name;
+      pasture.textContent = 'Take to Pasture';
+      pasture.addEventListener('click', goToPasture); 
+      ul.appendChild(pasture);
     }
     
   }
@@ -64,18 +91,18 @@ function Unicorn(name, color, food, location) {
 
 // calling constructor and storing new instance in variable 
 var unicornOne = new Unicorn('henry', 'blue', 'corn', 'barn');
-// var unicornTwo = new Unicorn('john', 'orange', 'apples', 'barn');
-// var unicornThree = new Unicorn('susan', 'green','grapes', 'barn');
-// var unicornFour = new Unicorn('jacky', 'red', 'bananas', 'barn');
+var unicornTwo = new Unicorn('john', 'orange', 'apples', 'pasture');
+var unicornThree = new Unicorn('susan', 'green','grapes', 'trail');
+var unicornFour = new Unicorn('jacky', 'red', 'bananas', 'barn');
 
 console.log(unicornOne);
 console.log(allUnicorns)
 
 // calling render method on instance to show unicorns
 unicornOne.render();
-// unicornTwo.render();
-// unicornThree.render();
-// unicornFour.render();
+unicornTwo.render();
+unicornThree.render();
+unicornFour.render();
 
 
 //maybe buttons as options
@@ -85,7 +112,7 @@ unicornOne.render();
 //css
 // stretch  db  talk to db  deply with links
 
-
+// event handler if pasture button is clicked.  changes location and rerenders 
 function goToPasture(event) {
 
   console.log('pasture')
@@ -114,6 +141,18 @@ function goToTrail(event) {
       console.log(allUnicorns)
       allUnicorns[i].render();
       
+    }
+  }
+}
+
+function goToBarn(event) {
+  var id = event.target.id;
+  for ( var i = 0; i < allUnicorns.length; i++) {
+    if( id === allUnicorns[i].name) {
+  
+      allUnicorns[i].location = 'barn';
+      console.log(allUnicorns)
+      allUnicorns[i].render();
     }
   }
 }
